0ff692bd68c6b9628cce98cc95e2c5262ebfb695
Arzena Akter <arzenaakter@gmail.com>
2023-06-10 03:32:46 +0600

    get data from server for manage classes dashboard

--- src/Pages/Dashboard/MangeClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/MangeClasses/ManageClasses.jsx
@@ -1,6 +1,19 @@
 
+import { useEffect } from "react";
+
 
 const ManageClasses = () => {
+
+ 
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/addClasses')
+        .then(res=> res.json())
+        .then(data => console.log(data))
+    },[])
+
+
+
     return (
         <div>
             manage classes
